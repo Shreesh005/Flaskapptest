2c3a154d4b1d2bb882c5d8a7e6e5b01c4efa8d85
Shreesh005 <69748625+Shreesh005@users.noreply.github.com>
2025-06-23 20:36:09 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Hello from Flask on Vercel!"})
 
-# Required by Vercel
+# Vercel entry point
 def handler(environ, start_response):
     return app(environ, start_response)
